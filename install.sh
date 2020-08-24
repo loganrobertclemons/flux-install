65caf2697f073409f0d13d0f596f1607c83b1cf3
Logan Clemons <loganrobertclemons@gmail.com>
2020-08-24 10:16:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 sudo chmod +x initKey.sh
 
+helm repo add fluxcd https://charts.fluxcd.io
+kubectl create namespace flux
+
 #installs flux and helm operator that will kick off server deployment
 ./initKey.sh
 ./installFlux.sh
